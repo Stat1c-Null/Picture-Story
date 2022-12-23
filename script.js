bcd292859c38a7ef8e58c474b727c77c3cc20c78
Mikita Slabysh <slabysh2015@gmail.com>
2022-12-23 00:44:29 -0500

    Added font, improved notes

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const noteOn = false;
+
 function hideNote() {
-    document.getElementById('note').style.display='none';
+    document.getElementById('note').style.visibility='hidden';
+    noteOn = false;
+};
+
+function showNote() {
+    document.getElementById('note').style.visibility='visible';
+    noteOn = true;
 }
--- upload-image.php
+++ upload-image.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
     <script src="script.js" type="text/javascript"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans:wght@400;700&display=swap" rel="stylesheet">
   </head>
   <body>
     <div id="container">
@@ -16,22 +17,25 @@
       <div id="upload">
         <form method="post" enctype="multipart/form-data">
           <input type="file" name="upload" accept=".png, .gif, .jpg" required>
-          <input type="submit" name="submit" value="Upload">
+          <input type="submit" name="submit" onclick="showNote()" value="Upload">
         </form>
       </div>
-      <div class="good" id="note" onmousedown="hideNote()">
+      <div class="good" id="note" onmousedown='hideNote()'>
         Succesfully Uploaded
       </div>
-      <div class="bad" id="note" onmousedown="hideNote()">
+      <!--<div class="bad" id="note" onmousedown='hideNote()'>
         Failed to Upload
-      </div>
+      </div>-->
     </div>
     <?php
-    // SAVE IMAGE INTO DATABASE
-    if (isset($_FILES["upload"])) {
-      require "backend/lib.php";
-      $_DBIMG->save($_FILES["upload"]["name"], file_get_contents($_FILES["upload"]["tmp_name"]));
-    }
+      // SAVE IMAGE INTO DATABASE
+      if (isset($_FILES["upload"])) {
+        require "backend/lib.php";
+        $_DBIMG->save($_FILES["upload"]["name"], file_get_contents($_FILES["upload"]["tmp_name"]));
+      } 
     ?>
+    <script>
+      
+    </script>
   </body>
 </html>
